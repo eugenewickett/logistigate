6f66b4083d370bdcd0b99e0f42f8a217439f3133
Eugene <eugenewickett@gmail.com>
2022-12-30 18:08:12 -0600

    dbug

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.1.1'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '4.0.2'
+needs_sphinx = '3.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
