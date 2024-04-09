3d3962d18eba87fc2329f2e381938b2ca61dd714
Eugene <eugenewickett@gmail.com>
2024-04-09 02:38:44 -0500

    move some OP functions

--- logistigate/orienteering.py
+++ logistigate/orienteering.py
@@ -60,3 +60,70 @@ def GetConstraintsWithPathCut(numVar, numTN, pathInd):
     newconstraintmat[0, numTN*3 + pathInd] = 1.
     return spo.LinearConstraint(newconstraintmat, np.ones(1), np.ones(1))
 
+
+def GetRegion(dept_str, dept_df):
+    """Retrieves the region associated with a department"""
+    return dept_df.loc[dept_df['Department']==dept_str,'Region'].values[0]
+
+
+def GetDeptChildren(reg_str, dept_df):
+    """Retrieves the departments associated with a region"""
+    return dept_df.loc[dept_df['Region']==reg_str, 'Department'].values.tolist()
+
+
+def GetSubtourMaxCardinality(optparamdict):
+    """Provide an upper bound on the number of regions included in any tour; HQ region is included"""
+    mincostvec = [] # initialize
+    dept_df = optparamdict['dept_df']
+    ctest, B, batchcost = optparamdict['pertestcost'], optparamdict['budget'], optparamdict['batchcost']
+    for r in range(len(optparamdict['regnames'])):
+        if r != optparamdict['reghqind']:
+            currReg = optparamdict['regnames'][r]
+            currmindeptcost = np.max(optparamdict['deptfixedcostvec'])
+            deptchildren = GetDeptChildren(currReg, dept_df)
+            for currdept in deptchildren:
+                currdeptind = optparamdict['deptnames'].index(currdept)
+                if optparamdict['deptfixedcostvec'][currdeptind] < currmindeptcost:
+                    currmindeptcost = optparamdict['deptfixedcostvec'][currdeptind]
+            currminentry = optparamdict['arcfixedcostmat'][np.where(optparamdict['arcfixedcostmat'][:, r] > 0,
+                                                                    optparamdict['arcfixedcostmat'][:, r],
+                                                                    np.inf).argmin(), r]
+            currminexit = optparamdict['arcfixedcostmat'][r, np.where(optparamdict['arcfixedcostmat'][r] > 0,
+                                                                    optparamdict['arcfixedcostmat'][r],
+                                                                    np.inf).argmin()]
+            mincostvec.append(currmindeptcost + currminentry + currminexit + ctest)
+        else:
+            mincostvec.append(optparamdict['reghqind']) # HQ is always included
+    # Now add regions until the budget is reached
+    currsum = 0
+    numregions = 0
+    nexttoadd = np.array(mincostvec).argmin()
+    while currsum + mincostvec[nexttoadd] <= B - batchcost:
+        currsum += mincostvec[nexttoadd]
+        numregions += 1
+        _ = mincostvec.pop(nexttoadd)
+        nexttoadd = np.array(mincostvec).argmin()
+
+    return numregions
+
+
+def GetUpperBounds(optparamdict, alpha=1.0):
+    """
+    Returns a numpy vector of upper bounds for an inputted parameter dictionary. alpha determines the proportion of the
+    budget that can be dedicated to any one district
+    """
+    B, f_dept, f_reg = optparamdict['budget']*alpha, optparamdict['deptfixedcostvec'], optparamdict['arcfixedcostmat']
+    batchcost, ctest, reghqind = optparamdict['batchcost'], optparamdict['pertestcost'], optparamdict['reghqind']
+    deptnames, regnames, dept_df = optparamdict['deptnames'], optparamdict['regnames'], optparamdict['dept_df']
+    retvec = np.zeros(f_dept.shape[0])
+    for i in range(f_dept.shape[0]):
+        regparent = GetRegion(deptnames[i], dept_df)
+        regparentind = regnames.index(regparent)
+        if regparentind == reghqind:
+            retvec[i] = np.floor((B-f_dept[i]-batchcost)/ctest)
+        else:
+            regfixedcost = f_reg[reghqind,regparentind] + f_reg[regparentind, reghqind]
+            retvec[i] = np.floor((B-f_dept[i]-batchcost-regfixedcost)/ctest)
+    return retvec
+
+
